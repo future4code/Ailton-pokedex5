61401ebbe5991c52cb456958b54bfcf094d45658
phcsilva056 <phc.silva@outlook.com.br>
2022-07-19 17:09:39 -0300

    continuando tela de detalhes

--- pokedex5/src/Components/Header/Header.js
+++ pokedex5/src/Components/Header/Header.js
@@ -1,21 +1,40 @@
-import React from 'react'
-import Logo from '../../assets/Logo.png'
-import Vetor from '../../assets/vetor.png'
-import { VetorImg, Voltar, ButtonHeader, DivHeader, HeaderContainer, ImgHeader } from './styled'
-import { Title } from '../../styleGeral'
+import React from "react";
+import Logo from "../../assets/Logo.png";
+import Vetor from "../../assets/vetor.png";
+import {
+  VetorImg,
+  Voltar,
+  ButtonHeader,
+  DivHeader,
+  HeaderContainer,
+  ImgHeader,
+} from "./styled";
+import { Title } from "../../styleGeral";
+import { useNavigate } from "react-router-dom";
+import {
+  goToDetailsPage,
+  goToHomePage,
+  goToPokedex,
+} from "../../routes/Coordinator";
 
 export default function Header() {
+  const navigate = useNavigate();
   return (
     <HeaderContainer>
       <DivHeader>
-        <Voltar><VetorImg src={Vetor}/>Todos os Pokémons</Voltar>
+        <Voltar onClick={() => goToHomePage(navigate)}>
+          <VetorImg src={Vetor} />
+          Todos os Pokémons
+        </Voltar>
       </DivHeader>
       <DivHeader>
-        <ImgHeader src={Logo} />
+        <ImgHeader onClick={() => goToDetailsPage(navigate, 1)} src={Logo} />
       </DivHeader>
       <DivHeader>
-        <ButtonHeader>Pokedéx</ButtonHeader>
+        <ButtonHeader onClick={() => goToPokedex(navigate)}>
+          Pokedéx
+        </ButtonHeader>
       </DivHeader>
     </HeaderContainer>
-  )
+  );
 }
